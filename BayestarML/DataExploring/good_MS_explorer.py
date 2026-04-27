9cf958962331b42ba8b3e2304d6b9551e0bfef54
Ginger Joe <josephbkershaw@gmail.com>
2026-04-27 17:24:25 +0200

    Mode and Type Histograms

--- BayestarML/DataExploring/good_MS_explorer.py
+++ BayestarML/DataExploring/good_MS_explorer.py
@@ -13,15 +13,17 @@ modes = df["mode"]
 pd.Series(modes).value_counts(dropna=False).plot(kind='bar')
 plt.xlabel("Detection Mode")
 plt.ylabel("Frequency")
-plt.savefig("modes_hist_MS.pdf")
+plt.savefig("DataExploring/modes_hist_MS.pdf")
 plt.show()
 
 #histogram of spectral types
 df["type"] = df["type"].str[0] #bit dodgy as one entry for example is F/G
+df["mode"] = df["mode"].replace("TEB", "EB")
+df["mode"] = df["mode"].replace("A/I", "A")
 sns.countplot(data=df, x='type', hue='mode', alpha=0.7)
-plt.xticks(rotation=45)
 plt.xlabel("Spectral Type")
 plt.ylabel("Frequency")
+plt.savefig("DataExploring/types_hist_MS.pdf")
 plt.show()
 
 #see test and train data
