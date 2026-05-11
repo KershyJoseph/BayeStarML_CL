1343acdf739fe2401c52ae544c2ce54b2d027cce
Ginger Joe <josephbkershaw@gmail.com>
2026-05-11 23:02:58 +0200

    60 advi

--- BayestarML/exec_trainer.py
+++ BayestarML/exec_trainer.py
@@ -313,7 +313,7 @@ if __name__ == '__main__':
     #HAVE YOU UPDATED CONSTANTS.PY
 
     #print("TRY 4 nodes on PREV RESULTS - simple 5_1000_4 with HalfNormal and 1.5*draw")
-    print("GP Mass train goodMS - ADVI - 55")
+    print("GP Mass train goodMS - ADVI - 60")
     mass_train_GP(55,55,advi=True)
     #radius_train_GP(60,60)
     #mass_train_SIMPLE_NN(4,1000,4)
