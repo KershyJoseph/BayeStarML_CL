e305a303ce4906aa6aec49f230447592e0d859f1
Ginger Joe <josephbkershaw@gmail.com>
2026-05-11 23:31:38 +0200

    Simple NN 6 node trial

--- BayestarML/exec_trainer.py
+++ BayestarML/exec_trainer.py
@@ -312,11 +312,10 @@ if __name__ == '__main__':
 
     #HAVE YOU UPDATED CONSTANTS.PY
 
-    #print("TRY 4 nodes on PREV RESULTS - simple 5_1000_4 with HalfNormal and 1.5*draw")
-    print("GP Mass train goodMS - ADVI - 65")
-    mass_train_GP(70,70,advi=True)
+    print("Mass train NN Simple try 6 nodes - 1.5 draw and C2Norm")
+    #mass_train_GP(70,70,advi=True)
     #radius_train_GP(60,60)
-    #mass_train_SIMPLE_NN(4,1000,4)
+    mass_train_SIMPLE_NN(6,1000,4)
     #radius_train_NN(5, 1000, 4)
 
     #from Gemini
