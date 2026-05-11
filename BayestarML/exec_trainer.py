29eaf3233810ed7c45cfcdb99547c9cb085d6be0
Ginger Joe <josephbkershaw@gmail.com>
2026-05-11 15:00:03 +0200

    ESS Inv #2

--- BayestarML/exec_trainer.py
+++ BayestarML/exec_trainer.py
@@ -304,11 +304,11 @@ if __name__ == '__main__':
 
     #HAVE YOU UPDATED CONSTANTS.PY
 
-    print("normal NN quick 200 on good MS to get ESS stats")
+    print("quick NN run with 3_100_2 on good MS to get ESS stats")
     # mass_train_GP(60,60)
     # radius_train_GP(60,60)
     #mass_train_SIMPLE_NN(5,5000,4)
-    radius_train_NN(15, 200, 2)
+    radius_train_NN(3, 100, 2)
 
     #from Gemini
     snapshot2 = tracemalloc.take_snapshot()
