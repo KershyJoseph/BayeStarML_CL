30ea8597eb7cce2063a844220ed9eb37cf3adcc1
Ginger Joe <josephbkershaw@gmail.com>
2026-05-16 19:40:47 +0200

    #3

--- BayestarML/exec_predictor.py
+++ BayestarML/exec_predictor.py
@@ -12,10 +12,11 @@ from predict import predict3, predict4
 def train_bart_bhs():
     #X, X_er = prepare_pred4("Datasets/dataset_A_trimmed_8cols_NASAFLAG.csv")
 
-    _, bhs_pred, bhs_w = predict4(target='mass',
+    _, bhs_pred, bhs_w = predict4(X=None, X_er=None,
+                                  target='mass',
                                   dataset_path="DataExploring/good_MS.txt",
                                   test=True) # disregards X, X_er for test=True / uses test values
     #_, pred, w4 = predict4(X, X_er, 'mass') # make predictions on new data (X, X_er)
 
 if __name__ == '__main__':
-    train_bart_bhs
+    train_bart_bhs()
