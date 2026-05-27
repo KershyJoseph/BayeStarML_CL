b4d272d3b75e7b64f6529f378523a731d5db0851
Ginger Joe <josephbkershaw@gmail.com>
2026-05-27 14:33:45 +0200

    pm.log_likelihood back

--- BayestarML/utils.py
+++ BayestarML/utils.py
@@ -109,8 +109,7 @@ def train(model, filename, draw=1000, chains=2,
     trace = pm.sample(draws=draw, tune=int(1.5*draw), chains=chains,
                       cores=chains, model=model, target_accept=target_accept,
                       max_treedepth=max_treedepth,
-                      nuts_sampler="nutpie",
-                      idata_kwargs={"log_likelihood": True})
+                      nuts_sampler="nutpie")
 
     with pd.option_context("display.max_rows", None):
         df = az.summary(trace)
@@ -122,9 +121,9 @@ def train(model, filename, draw=1000, chains=2,
     for var in r_hat_values.data_vars:
         max_rhat = r_hat_values[var].max().values.item()
         all_rhats.append((var, max_rhat))
-
     print("rhats: ", all_rhats)
 
+    trace.extend(pm.compute_log_likelihood(trace, model=model, var_names='y')) 
     print("loo trace: ", az.loo(trace))
 
     trace.to_netcdf(filename)
