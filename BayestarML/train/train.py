1c470cd2b26bedf6c5ebd36e43a828d3bc73ff66
Ginger Joe <josephbkershaw@gmail.com>
2026-06-18 16:15:20 +0200

    NN M 700ms 0.5He

--- BayestarML/train/train.py
+++ BayestarML/train/train.py
@@ -171,7 +171,7 @@ def train_NN(
 ):
     """Function to train HBNN"""
     hyperp_str = (
-        "NN_0.1He_"
+        "NN_0.5He_"
         + target
         + "_"
         + dataset_key
@@ -235,9 +235,9 @@ if __name__ == "__main__":
     start_time_CPU = time.process_time()
     start_time_wall = time.perf_counter()
 
-    print("700ms mass NN. 0.1*He priors. 0.2 non-centred er prior. 0.01 bias priors. NUTPIE with init_means. 8_2000")
+    print("700ms M NN. 0.5*He priors. 0.2 non-centred er prior. 0.05 bias priors. NUTPIE with init_means. 4_2000")
     train_NN(
-        "700ms", "M", 8, draws=2000, nutpie=True
+        "700ms", "M", 4, draws=2000, nutpie=True
     )
 
     end_time_CPU = time.process_time()
