5dc7609711d1257acc78776cf7ff39fc4f246897
Ginger Joe <josephbkershaw@gmail.com>
2026-06-19 22:56:24 +0200

    GP R no overfitting

--- BayestarML/train/train.py
+++ BayestarML/train/train.py
@@ -235,9 +235,9 @@ if __name__ == "__main__":
     start_time_CPU = time.process_time()
     start_time_wall = time.perf_counter()
 
-    print("700ms R NN, 1 layer. 0.05/0.1 in/out ws and biases 0.2. LOG_ER at -1.5, 0.5. 4_2000")
-    train_NN_1layer(
-        "700ms", "R", 4, draws=2000
+    print("700ms R GP, 1 layer. 10_10_2000")
+    train_GP(
+        "700ms", "R", 10, 10, draws=2000
     )
 
     end_time_CPU = time.process_time()
