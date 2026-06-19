21972e7df02e42b7687446e3b44b34eb8bd7fe59
Ginger Joe <josephbkershaw@gmail.com>
2026-06-19 17:39:48 +0200

    #2

--- BayestarML/src/models/gp.py
+++ BayestarML/src/models/gp.py
@@ -302,34 +302,8 @@ def sparse_fully_heteroscedastic_gp(
         # ls = pm.InverseGamma("ls", mu=np.log(ls_mu_vec), sigma=ls_sd_vec, shape=D)
 
         #Try not having data-driven priors on lengthscale. Also LogNormal instead of InverseGamma. 
-        log_ls = pm.Normal("log_ls", mu=0.5, sigma=0.5, shape=D) #logR relations are quite linear
-        ls = pm.Deterministic("ls", pm.math.exp(log_ls))
-        #eta = pm.Gamma("eta", alpha=2, beta=1)
-        log_eta = pm.Normal("log_eta", mu=0.4, sigma=0.4)
-        eta = pm.Deterministic("eta", pm.math.exp(log_eta))
-
-        cov_mean = eta**2 * pm.gp.cov.ExpQuad(input_dim=D, ls=ls) \
-                   + pm.gp.cov.WhiteNoise(sigma=1e-5)
-
-        μ_gp = SparseLatent(cov_mean)
-        μ_f, μ_trace = μ_gp.prior("μ", X_mu, Xu)
-
-        X_var_data = pm.Data("X_var", X_var)
-        D_var = X_var.shape[1]
-
-        # Priors for ARD lengthscales over concatenated space
-        # ls_v_mu_list, ls_v_sd_list = [], []
-        # for d in range(D_var):
-        #     μ_d, σ_d = get_ℓ_prior(X_var[:, d])
-        #     ls_v_mu_list.append(μ_d)
-        #     ls_v_sd_list.append(σ_d)
-
-        # ls_v_mu_vec = np.array(ls_v_mu_list)
-        # ls_v_sd_vec = np.array(ls_v_sd_list)
-
         log_ls = pm.Normal("log_ls", mu=0.6, sigma=0.5, shape=D)
         ls = pm.Deterministic("ls", pm.math.exp(log_ls))
-        #eta = pm.Gamma("eta", alpha=2, beta=1)
         log_eta = pm.Normal("log_eta", mu=0.5, sigma=0.4)
         eta = pm.Deterministic("eta", pm.math.exp(log_eta))
 
@@ -352,7 +326,6 @@ def sparse_fully_heteroscedastic_gp(
         # ls_v_mu_vec = np.array(ls_v_mu_list)
         # ls_v_sd_vec = np.array(ls_v_sd_list)
 
-        # ls_v = pm.InverseGamma("ls_var", mu=ls_v_mu_vec, sigma=ls_v_sd_vec, shape=D_var)
         log_ls_v = pm.Normal("log_ls_v", mu=-0.8, sigma=0.4, shape=D_var)
         ls_v = pm.Deterministic("ls_v", pm.math.exp(log_ls_v))
         log_eta_v = pm.Normal("log_eta_v", mu=0.0, sigma=0.5)
@@ -360,6 +333,12 @@ def sparse_fully_heteroscedastic_gp(
 
         cov_var = eta_v**2 * pm.gp.cov.ExpQuad(input_dim=D_var, ls=ls_v) \
                   + pm.gp.cov.WhiteNoise(sigma=1e-5)
+
+        μ_gp = SparseLatent(cov_mean)
+        μ_f, μ_trace = μ_gp.prior("μ", X_mu, Xu)
+
+        X_var_data = pm.Data("X_var", X_var)
+        D_var = X_var.shape[1]
 
         alpha_log_var = pm.Normal("alpha_log_var", mu=0.0, sigma=1.0)
         log_var_gp = SparseLatent(cov_var)
