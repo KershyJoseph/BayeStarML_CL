79c45f7b67944f3e78620fe15ea3d9d571b6bee8
Ginger Joe <josephbkershaw@gmail.com>
2026-06-21 01:30:38 +0200

    GP R 700ms 50_50_1000

--- BayestarML/src/models/gp.py
+++ BayestarML/src/models/gp.py
@@ -303,7 +303,7 @@ def sparse_fully_heteroscedastic_gp(
         # ls = pm.InverseGamma("ls", mu=np.log(ls_mu_vec), sigma=ls_sd_vec, shape=D)
 
         #Try not having data-driven priors on lengthscale. Also LogNormal instead of InverseGamma. 
-        log_ls = pm.Normal("log_ls", mu=-0.5, sigma=0.2, shape=D)
+        log_ls = pm.Normal("log_ls", mu=-0.8, sigma=0.2, shape=D)
         ls = pm.Deterministic("ls", pm.math.exp(log_ls))
         log_eta = pm.Normal("log_eta", mu=-0.5, sigma=0.3)
         eta = pm.Deterministic("eta", pm.math.exp(log_eta))
@@ -311,7 +311,7 @@ def sparse_fully_heteroscedastic_gp(
         k_sq_exp = eta**2 * pm.gp.cov.ExpQuad(input_dim=D, ls=ls)
         cov_mean = k_sq_exp + pm.gp.cov.WhiteNoise(sigma=1e-5)
         if linear_kernel:
-            grad_var = pm.HalfNormal("grad_var", sigma=0.5, shape=D)
+            grad_var = pm.HalfNormal("grad_var", sigma=0.7, shape=D)
             k_linear = pm.gp.cov.Linear(input_dim=D, c=grad_var)
             cov_mean = cov_mean + k_linear
 
@@ -334,7 +334,7 @@ def sparse_fully_heteroscedastic_gp(
 
         log_ls_v = pm.Normal("log_ls_v", mu=-0.2, sigma=0.2, shape=D_var)
         ls_v = pm.Deterministic("ls_v", pm.math.exp(log_ls_v))
-        log_eta_v = pm.Normal("log_eta_v", mu=-0.7, sigma=0.3)
+        log_eta_v = pm.Normal("log_eta_v", mu=-0.8, sigma=0.2)
         eta_v = pm.Deterministic("eta_v", pm.math.exp(log_eta_v))
 
         cov_var = eta_v**2 * pm.gp.cov.ExpQuad(input_dim=D_var, ls=ls_v) \
